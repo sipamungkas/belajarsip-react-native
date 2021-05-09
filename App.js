005fb32d97de03b60ff11d052b34dbd38cddbc2f
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-09 21:59:59 +0700

    feat: add Bottom Tab Navigator

--- App.js
+++ App.js
@@ -2,20 +2,72 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
+import DashboardStudent from './src/screens/Dashboard/Student';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Dashboard') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person-circle' : 'person-circle-outline';
+          } else if (route.name === 'Chat') {
+            iconName = focused
+              ? 'chatbox-ellipses'
+              : 'chatbox-ellipses-outline';
+          } else if (route.name === 'Activity') {
+            iconName = focused ? 'bookmark' : 'bookmark-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'rgba(87, 132, 186, 1)',
+        inactiveTintColor: 'rgba(173, 169, 187, 1)',
+      }}>
+      <Tab.Screen name="Dashboard" component={DashboardStudent} />
+      <Tab.Screen name="Activity" component={DashboardStudent} />
+      <Tab.Screen name="Chat" component={DashboardStudent} />
+      <Tab.Screen name="Profile" component={DashboardStudent} />
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator headerMode={'none'} initialRouteName="Register">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Home" component={TabNavigator} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
---
+++ src/screens/Dashboard/Student/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function DashboardStudent(props) {
+  return (
+    <View>
+      <Text>Dashboard Student</Text>
+    </View>
+  );
+}
