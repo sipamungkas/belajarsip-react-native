ff8f056bb4bacc1a09283605e5480d00c3750dc4
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-10 23:51:36 +0700

    refactor: remove console.log

--- src/components/Activity/AvailableClassList/index.js
+++ src/components/Activity/AvailableClassList/index.js
@@ -34,7 +34,6 @@ export default function AvailableClassList(props) {
     setCurrentPage(currentPage - 1);
   };
   const nextPageHandler = () => {
-    console.log(currentPage);
     if (currentPage === totalPage) {
       return;
     }
