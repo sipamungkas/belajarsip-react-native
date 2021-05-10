db91f6ea0610283e23b91e7dedbec8f42961fb92
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-10 08:36:12 +0700

    feat: dashboard peserta implementation

--- src/components/Dashboard/ForYou/index.js
+++ src/components/Dashboard/ForYou/index.js
@@ -1,39 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import ProgressCircle from 'react-native-progress-circle';
-import {Card} from 'react-native-paper';
+import {ActivityIndicator, Card} from 'react-native-paper';
+import axios from 'axios';
+
 import styles from './styles';
 
-export default function index(props) {
+import {API_URL} from '@env';
+import Color from '../../../Color';
+
+export default function ForYou(props) {
+  const [myClassData, setMyClassData] = useState();
+  const date = '2021-03-29';
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJuYW1lIjoiQnVyaGFuIFVwZGF0ZWEiLCJyb2xlX2lkIjoyLCJpYXQiOjE2MjA2MDk3MDksImV4cCI6MTYyMDY5NjEwOSwiaXNzIjoiQkVMQUpBUlNJUCJ9.04ndNaHCYYS_bfaW8Hb1vLD6cvmgQAZ-q-ADOZsC84U';
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/v1/dashboard/${date}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(res => {
+        setMyClassData(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [token, date]);
+
   return (
     <View>
-      {props.data.map((item, index) => (
-        <Card
-          key={item.id}
-          elevation={2}
-          style={{marginVertical: 3}}
-          theme={{roundness: 10}}>
-          <TouchableOpacity style={styles.item}>
-            <View style={{flex: 1}}>
-              <Text style={styles.time}> {item.time || '00.00 - 00.00'}</Text>
-            </View>
-            <View style={{...styles.titleContainer, flex: 2}}>
-              <Text style={styles.title}>{item.title || 'untitled'}</Text>
-            </View>
-            <View style={{flex: 1, alignItems: 'center'}}>
-              <ProgressCircle
-                percent={item.progress}
-                radius={20}
-                borderWidth={3}
-                color="#3399FF"
-                shadowColor="#fff"
-                bgColor="#fff">
-                <Text style={{fontSize: 14}}>{`${item.progress}%`}</Text>
-              </ProgressCircle>
-            </View>
-          </TouchableOpacity>
-        </Card>
-      ))}
+      {!myClassData && <ActivityIndicator color={Color.PRIMARY} />}
+      {myClassData &&
+        myClassData.map((item, index) => (
+          <Card
+            key={item.id}
+            elevation={2}
+            style={{marginVertical: 3}}
+            theme={{roundness: 10}}>
+            <TouchableOpacity style={styles.item}>
+              <View style={{flex: 1}}>
+                <Text style={styles.time}>
+                  {' '}
+                  {item.start_at || '00.00 - 00.00'}
+                </Text>
+              </View>
+              <View style={{...styles.titleContainer, flex: 2}}>
+                <Text style={styles.title}>{item.title || 'untitled'}</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <ProgressCircle
+                  percent={item.progress | 50}
+                  radius={20}
+                  borderWidth={3}
+                  color="#3399FF"
+                  shadowColor="#fff"
+                  bgColor="#fff">
+                  <Text style={{fontSize: 14}}>{`${item.progress | 50}%`}</Text>
+                </ProgressCircle>
+              </View>
+            </TouchableOpacity>
+          </Card>
+        ))}
     </View>
   );
 }
