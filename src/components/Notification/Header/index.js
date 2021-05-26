73641195b04a6933bc31416a9f9f1abda055d794
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-26 20:37:45 +0700

    feat: add notification header component

--- src/components/Notification/Header/index.js
+++ src/components/Notification/Header/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import {View, Text, StatusBar} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 import Color from '../../../Color';
 import styles from './styles';
 
-export default function Header(props) {
+export default function Header() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor={Color.PRIMARY} />
       <View style={styles.main}>
-        <Ionicons
-          name="chevron-back"
-          size={30}
-          color="white"
-          onPress={() => {}}
-        />
         <Text numberOfLines={1} style={styles.title}>
           Notification
         </Text>
@@ -23,7 +19,9 @@ export default function Header(props) {
           name="close-circle"
           size={30}
           color="white"
-          onPress={() => {}}
+          onPress={() => {
+            navigation.navigate('Dashboard');
+          }}
         />
       </View>
     </View>
