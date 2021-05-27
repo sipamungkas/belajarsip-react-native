8b9af73e6ce809ab9d551987bc00a6f0c73ccb2b
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-27 09:03:59 +0700

    fix: add key on notification

--- src/screens/Notification/index.js
+++ src/screens/Notification/index.js
@@ -4,23 +4,28 @@ import Header from '../../components/Notification/Header';
 import NotificationItem from '../../components/Notification/NotificationItem';
 import styles from './styles';
 
+let id = 0;
 const today = [
   {
+    id: id++,
     image: 'hot',
     content: 'There are 10 news update for today. Don’t miss it!',
     time: '2 min',
   },
   {
+    id: id++,
     image: 'avatar',
     content: 'Nissa Sabyan sent you a message',
     time: '2 min',
   },
   {
+    id: id++,
     image: 'avatar',
     content: 'Rio Dewanto sent you a message',
     time: '2 min',
   },
   {
+    id: id++,
     image: 'activity',
     content: 'You have 2 classes today.',
     time: '15 hr',
@@ -29,21 +34,25 @@ const today = [
 
 const yesterday = [
   {
+    id: id++,
     image: 'hot',
     content: 'There are 10 news update for today. Don’t miss it!',
     time: 'yesterday',
   },
   {
+    id: id++,
     image: 'avatar',
     content: 'Nissa Sabyan sent you a message',
     time: 'yesterday',
   },
   {
+    id: id++,
     image: 'avatar',
     content: 'Rio Dewanto sent you a message',
     time: 'yesterday',
   },
   {
+    id: id++,
     image: 'activity',
     content: 'You have 2 classes today.',
     time: 'yesterday',
@@ -59,6 +68,7 @@ export default function Notification() {
         <View style={styles.notificationContainer}>
           {today.map((item, index) => (
             <NotificationItem
+              key={index}
               content={item.content}
               time={item.time}
               image={item.image}
@@ -69,6 +79,7 @@ export default function Notification() {
         <View style={styles.notificationContainer}>
           {yesterday.map((item, index) => (
             <NotificationItem
+              key={index}
               content={item.content}
               time={item.time}
               image={item.image}
@@ -79,6 +90,7 @@ export default function Notification() {
         <View style={styles.notificationContainer}>
           {yesterday.map((item, index) => (
             <NotificationItem
+              key={index}
               content={item.content}
               time={item.time}
               image={item.image}
